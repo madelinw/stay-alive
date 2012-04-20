60d75c023851485469b25f1c6630b8a97d84022d
Miss Woods <misswoods@LappersTop.local>
2012-04-19 23:02:05 -0700

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  attr_accessor :password
+  attr_protected :password_digest
+
+  validates :name, :presence => true
+  validates :email, :presence => true, :uniqueness => true, :email => true
+  validates :password, :presence => true, :confirmation => true
+  validates :password_confirmation, :presence => { :if => :password }
+  validates :phone, :format => { :allow_nil => true, :with => /^[()0-9- +.]{10,20} *[extension.]{0,9} *[0-9]{0,5}$/i }
+
+  def password=(pass)
+    return if pass.blank?
+    @password = pass
+    self.password_digest = BCrypt::Password.create(pass)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '8e7f151ff2f4b9e6a86f631c8b0cb566e50429c3a13f8fcb391129f870c2f393e2afda2e4c122367ad4ff62910b20210e690af8ee41aeef62646b16a4772bcb2'
