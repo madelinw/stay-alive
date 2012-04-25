95d381a75b79e9894f6e3490513f6e247923bc87
Miss Woods <madelinw@gmail.com>
2012-04-25 00:40:12 -0700

    create & define login view, begin using sass

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ group :assets do
   gem 'sass-rails',   '~> 3.1.4'
   gem 'coffee-rails', '~> 3.1.1'
   gem 'uglifier', '>= 1.0.3'
+  gem 'compass-rails', '1.0.1'
 end
 
 gem 'jquery-rails'
---
+++ app/assets/javascripts/views/loginView.js
@@ -0,0 +1,44 @@
+LoginView = Backbone.View.extend({
+  el: '#login-view',
+  events: { 'submit form': 'authorize' },
+
+  authorize: function() {
+    if (this.submitButton.hasClass('disabled') && !(this.form.data('user-authorized') === true)) {
+      return false;
+    } else {
+      this.submitButton.addClass('disabled');
+    }
+
+    var self = this,
+      attrs = {
+        email: this.emailField.val(),
+        password: this.passwordField.val()
+      };
+    User.authorize(attrs, function(err, user) {
+      if (err) { 
+        self.loginFailure();
+      } else {
+        self.loginSuccess();
+      }
+    });
+    return (this.form.data('user-authorized') === true);
+  },
+
+  loginSuccess: function() {
+    this.form.data('user-authorized', true);
+    this.form.submit();
+  },
+
+  loginFailure: function() {
+    this.$el.addClass('squiggle');
+    this.emailField.focus();
+    this.submitButton.removeClass('disabled');
+    this.removeClass('squiggle');
+  }
+
+  initialize: function() {
+    this.form = this.$el.find('form');
+    this.emailField = this.$el.find('input[name=email]');
+    this.passwordField = this.$el.find('input[type=submit]');
+  }
+})
